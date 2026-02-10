b7d347e65c164cd875558f38b8a780b7012e3c57
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-02-10 14:22:42 +0000

    chore(cli): add missing license headers
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- include/driver/validate.h
+++ include/driver/validate.h
@@ -1,3 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2024 Second State INC
+
+//===-- wasmedge/driver/validate.h - Validate entrypoint ------------------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contents the entrypoint for the validate executable.
+///
+//===----------------------------------------------------------------------===//
 #pragma once
 
 #include "po/argument_parser.h"
--- lib/driver/validate.cpp
+++ lib/driver/validate.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2024 Second State INC
+
 #include "driver/validate.h"
 #include "common/configure.h"
 #include "common/filesystem.h"
